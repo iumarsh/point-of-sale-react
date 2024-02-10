9498a49f91c193c745a20ac10fd7bc4f55a9dc9d
Umar Farooq <umar.farooq@devigital.com>
2024-02-10 19:32:09 +0500

    Added the cheques details

--- src/scenes/cheques/index.js
+++ src/scenes/cheques/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { Box, Typography, useTheme, TextField, Autocomplete, Button, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
+import Header from "../../components/Header";
+import { useNavigate } from 'react-router-dom';
+import { BASEURL } from '../../data/endpoints';
+import { tokens } from '../../theme';
 const Cheques = () => {
-  return (
-    <div>Cheques</div>
-  )
-}
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-export default Cheques
+ 
+  return (
+    <Box m="20px">
+      <Header title="Mahmood Dari House" subtitle="Cheques Details" />
+      Coming Soon...
+    </Box>
+  );
+};
+
+export default Cheques;
