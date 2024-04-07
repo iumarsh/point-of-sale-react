b37ae29af9d826eb48850d9faf71cf50f45cbd52
Umar Farooq <umar.farooq@devigital.com>
2024-04-08 03:15:04 +0500

    Added the Pack Count in PDF

--- src/scenes/transactions/edit.jsx
+++ src/scenes/transactions/edit.jsx
@@ -227,7 +227,7 @@ const EditTransaction = () => {
                 name: x.Description,
                 quantity: x.Quantity,
                 type: x.UnitOfMeasurement,
-                than: x.UnitOfMeasurement === "meter" ? x.Than : 1,
+                than: x.UnitOfMeasurement === "meter" ? x.Than : x.Quantity,
                 uom: x.UnitOfMeasurement === "meter" ? 'Th' : x.UnitOfMeasurement,
                 price: x.Price,
                 total: x.Total,
@@ -252,7 +252,9 @@ const EditTransaction = () => {
         // doc.text('2. Payment is due within 30 days.', 20, doc.autoTable.previous.finalY + 50);
 
         // Add signature line
+        let _packTotal = result.reduce((total, item) => total + (item.UnitOfMeasurement === "meter" ? parseInt(item.Than) : parseInt(item.Quantity)), 0);
         doc.setFontSize(11);
+        doc.text(`Pack Total: ${_packTotal}`, 20, doc.autoTable.previous.finalY + 35);
         doc.text('Signature', 20, doc.autoTable.previous.finalY + 60);
         doc.text('Cash Received By: ', 20, doc.autoTable.previous.finalY + 70);
 
